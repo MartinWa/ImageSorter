670428d1de5df4d9d17a91c7affd5d0cefd4ce11
Martin Wåger <martin.w@comaround.se>
2016-10-30 11:53:54 +0100

    Start

--- ImageSorter/Program.cs
+++ ImageSorter/Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using ExifLib;
 
 namespace ImageSorter
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        //   private const string OnedriveFolder = @"F:\OneDrive\Bilder";
+        private const string OnedriveFolder = @"F:\test\original";
+        //   private const string ResultFolder = @"F:\sorted";
+        private const string ResultFolder = @"F:\test\sorted";
+
+        private const string FolderFormat = "{0}-{1}";
+
+        private static void Main()
         {
+            var files = Directory.GetFiles(OnedriveFolder, "*.jpg", SearchOption.AllDirectories);
+            var i = 1;
+            var total = files.Length;
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Copying file {i}/{total} - {file}");
+                using (var exifReader = new ExifReader(file))
+                {
+                    DateTime datePictureTaken;
+                    if (!exifReader.GetTagValue(ExifTags.DateTimeDigitized, out datePictureTaken))
+                    {
+                        Console.WriteLine("Can not read EXIF from file");
+                        continue;
+                    }
+                    var folderName = GetFolderName(datePictureTaken);
+                    var formattableString = $"{ResultFolder}\\{folderName}";
+                    Directory.CreateDirectory(formattableString);
+                    var newFileName = $"{Guid.NewGuid():N}.jpg";
+                    var destination = Path.Combine(formattableString, newFileName);
+                    File.Copy(file, destination);
+                }
+            i++;
+            }
+        }
+
+        private static string GetFolderName(DateTime dateTime)
+        {
+            return string.Format(FolderFormat, dateTime.Year, dateTime.ToString("MM"));
         }
     }
 }
