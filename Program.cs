2550030841e8612766326367e1757cd6c0197ff0
Martin Wåger <martinw@gmail.com>
2024-04-17 21:58:12 +0200

    Net 8

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using MetadataExtractor.Formats.Exif;
 using MetadataExtractor.Formats.FileSystem;
 using MetadataExtractor.Formats.FileType;
